af84644993a0beb23ebc2a57b1fe2d3325029352
Lera <mssolipsismus@gmail.com>
2023-11-13 14:54:18 +0100

    add application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.4"
+    application
+    id("org.springframework.boot") version "2.6.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.spring") version "1.5.21"
     id("com.google.cloud.tools.jib") version "3.1.4"
+}
+
+application {
+    mainClass.set("project.code.AppApplication")
 }
 
 val scalaBinaryVersion = "2.13"
